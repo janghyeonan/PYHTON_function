e965e955f0c1b41528e6fecbe27733d84a18498c
janghyeonan <ajh910@gmail.com>
2017-01-18 18:12:30 +0900

    (파이썬):텍스트 파일 읽은 후 print로 보여주기

---
+++ Python_Textfile_Read.py
@@ -0,0 +1,10 @@
+﻿#텍스트 파일 읽은 후 print로 보여주기
+
+f = open(r"C:\폴더경로\테스트파일명.txt", 'r')
+while True:
+    line = f.readline()
+    if not line: break
+    if line == '\n':
+        print('nonono')
+    print(line )
+f.close()
